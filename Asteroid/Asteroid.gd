b2957424588105a0787e62b8fd6f67557a39e44e
Jason Francis <jarfranc@iu.edu>
2022-09-13 15:20:46 -0400

    Initial commit

---
+++ Asteroid/Asteroid.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var initial_speed = 3.0
+
+func _ready():
+	velocity = Vector2(0,initial_speed*randf()).rotated(PI*2*randf())
+
+func _physics_process(_delta):
+	position = position + velocity
+	position.x = wrapf(position.x, 0, 1024)
+	position.y = wrapf(position.y, 0, 600)
